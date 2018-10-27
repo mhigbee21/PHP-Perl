e816abafa569be5c05679b030e0caae56d88801f
root <root@localhost.localdomain>
2018-10-27 20:42:39 +0000

    added config, database and memcache support

--- PHP/Perl.pm
+++ PHP/Perl.pm
@@ -1,20 +1,41 @@
 package PHP::Perl;
 use strict;
 use warnings;
-our( %in );
+our( %in, $dbh, $cache, $config );
 use Apache2::Const qw(FORBIDDEN OK);
 use Apache2::RequestUtil;
 use Data::Dumper;
 use Carp;
 use IO::String;
-
+use DBI;
+use Cache::Memcached;
+	
 sub handler
 {
 	my $r = shift;
 
 	return FORBIDDEN if ( $r->filename() !~ /\.perl$|\.pl/i );
-	
-	# reset for each request 
+
+	# define path to webconfig in httpd.conf
+	# PerlSetVar webconfig /{filepath}/web.config
+	my $webconfig = $r->dir_config('webconfig');
+
+	if( d$webconfig )
+	{
+		$config ||= parseWebConfig( $webconfig );
+		#warn Dumper $config;
+		
+		if( exists $config->{'memcached'} )
+		{ 
+			$cache = new Cache::Memcached { 'servers' => [$config->{'memcached'}] };
+		}	
+
+		if( exists $config->{database} )
+		{
+			$dbh ||= dbconnect( $config );
+		}
+	}
+
 	%in = ();	
  
 	if( $r->method eq 'POST' )
@@ -79,9 +100,8 @@ sub process
 		$html =~ m/<\?perl(.*?)\?>/s;
                 my $code = $1;
 		
-		# redirect STDOUT to a scalar...
+		# set STDOUT to a scalar...
 		my $result;
-
   		my $str_fh = IO::String->new($result);
   		my $old_fh = select($str_fh);
 	
@@ -92,7 +112,7 @@ sub process
 
                 if( $@ )
                 {
-                        carp( "$@" );
+                        Carp::cluck( "$@" );
                         $html =~ s/<\?perl(.*?)\?>/$@/s;
                 }
                 else
@@ -115,18 +135,60 @@ sub getfile
 		local $/;	
 		open(my $FILE, "<", "$path$file") || carp "Can't Open file $path$file $!";
 		my $contents = <$FILE>;
+		close( $FILE );
 		return $contents;
 	}
 
 	return "The file " . "$path$file" . " doesn't exist"; 
 }
 
+sub parseWebConfig
+{
+	my $file = shift;
+
+	my $conf = {};
+
+	if ( -e $file  )
+	{
+		open( my $fh, '<', $file ); 
+
+		while ( <$fh> )
+		{
+			$_ =~ s/(.*)\#.*/$1/;
+			$_ =~ s/^\s*(.*?)\s*$/$1/;
+			my ($l,$r) = split('=',$_) if (/=/);
+			chomp $r;
+			$conf->{$l} = $r if ($l);
+		}
+		close( $fh );
+	}
+	else
+	{
+		croak "The file $file doesn't exist";
+	}
+
+	return $conf;
+}
+
+sub dbconnect
+{
+	my $conf = shift;
+
+	$conf->{'driver'} = "DBI:mysql" unless $conf->{'driver'};	
+	
+	return DBI->connect("$conf->{'driver'}:$conf->{'database'};host=$conf->{'dbhost'}"
+		,"$conf->{'dbuser'}","$conf->{'dbpasswd'}"
+		,{AutoCommit=>1,RaiseError=>1}) || carp("$DBI::errstr");	
+}
+
+
 =pod
 	
 	Quick START
 	
 	Add this to apache conf.
-	
+
+  PerlSetVar webconfig /{filepath}/web.config	
   PerlModule PHP::Perl
   <LocationMatch ^/([^?.&=%+]+).pl$ >
         SetHandler perl-script
